b75bdcad95a103747ec8869ff4ac55815bf63110
syedaqhajisthafathima <syedaqhajisthafathima@gmail.com>
2026-01-01 17:52:20 +0530

    Stack_using_array.java

--- Phase-2/STACK/Stack_using_array.java
+++ Phase-2/STACK/Stack_using_array.java
@@ -1,1 +1,36 @@
+Class Stack{
+  int arr[];
+  int top;
+  public stack(int n){
+    arr=new int[n];
+    top=-1;
+  }
+  boolean push(int ele){
+    if(top==arr.length-1){
+      return false;//overflow
+    }
+    arr[++top]=ele;
+    return true;
+  }
+  int pop(){
+    if(top==-1)return -1;//underflow
+    int ele=arr[top--];
+    return ele;
+  }
+  int size(){
+    return top+1;
+  }
+  int peek(){
+    if(top==-1) return -1;
+    return arr[top];
+  }
+  booelan isEmpty(){
+    return top==-1;
+  }
+  boolean isFull(){
+    return top==arr.length-1;
+  }
+}
+  
 
+  
