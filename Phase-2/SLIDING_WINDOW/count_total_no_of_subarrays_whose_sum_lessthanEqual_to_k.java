c7db0a81d0fe6b51b28902f83fa13af90af9ebd9
syedaqhajisthafathima <syedaqhajisthafathima@gmail.com>
2026-01-06 23:14:26 +0530

    Update count_total_no_of_subarrays_whose_sum_lessthanEqual_to_k.java

--- Phase-2/SLIDING_WINDOW/count_total_no_of_subarrays_whose_sum_lessthanEqual_to_k.java
+++ Phase-2/SLIDING_WINDOW/count_total_no_of_subarrays_whose_sum_lessthanEqual_to_k.java
@@ -1,1 +1,14 @@
-
+int segmentwithsmallsum(int []arr,int k){
+  long sum=0;
+  int count=0;
+  int s=0;
+  for(int e=0;e<arr.length;e++){
+    sum+=arr[e];
+    while(sum>k&& s<m ){
+      sum -= arr[s];
+      s++;
+    }
+    count+=(e-s+1);
+  }
+  return len;
+}
